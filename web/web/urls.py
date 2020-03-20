513e306854a389ac9315bed82842522282c102f6
unhochoi <yms04089@kookmin.ac.kr>
2020-03-20 17:53:52 +0900

    Django Skeleton code
    Django basic environment

---
+++ web/web/urls.py
@@ -0,0 +1,22 @@
+"""web URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^', include('mainapp.urls')), #localhost:8000으로 요청이 들어오면 mainapp.urls로 전달
+    url(r'^admin/', admin.site.urls), #app 접속을 위해 include를 씁니다.
+]
