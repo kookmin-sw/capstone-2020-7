c88591f0d286b771602f27f4e2474347d6053514
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2020-04-06 01:20:53 +0900

    issue number #9 login

--- web/mainapp/urls.py
+++ web/mainapp/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views #.은 현재 폴더(elections)를 의미합니다.
+from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.index), #위의 urls.py와는 달리 include가 없습니다.
+    path('login/', views.login),
+    path('admin/', admin.site.urls),
 ]
--- web/mainapp/views.py
+++ web/mainapp/views.py
@@ -1,5 +1,29 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 def index(request):
     return render(request, 'mainapp/index.html')
+
+@csrf_exempt
+def login(request):
+
+    if request.method == 'POST':
+        #print("리퀘스트 로그" + str(request.body))
+        id = request.POST.get('userid','')
+        pw = request.POST.get('userpw', '')
+        #print("id = " + id + " pw = " + pw)
+
+        result = authenticate(username=id, password=pw)
+
+        if result :
+            print("로그인 성공!")
+            return HttpResponse(status=200)
+        else:
+            print("실패")
+            return HttpResponse(status=401)
+
+
+    return render(request, 'mainapp/login.html')
