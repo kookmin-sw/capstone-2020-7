e3714d8a01961474ffda67bd5393ca0e612f8676
unhochoi <yms04089@kookmin.ac.kr>
2020-04-08 13:15:50 +0900

    #16 IP De-identification3
    #16 IP De-identification3

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = '*i+$i90szo(10hg@l_$f-opbawhp%b^dhr+3(0lffjydj&n(zx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['INPUT_YOUR_IP']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -48,7 +49,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'web.urls'
 
-ALLOWED_HOSTS = ['INPUT_YOUR_IP']
 
 TEMPLATES = [
     {
