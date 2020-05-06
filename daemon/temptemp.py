b844472e9114831f816eca9575974247eeca32c7
unhochoi <yms04089@kookmin.ac.kr>
2020-05-06 21:15:05 +0900

    #36 Modified daemon with hahoon

--- daemon/temptemp.py
+++ daemon/temptemp.py
@@ -2,15 +2,18 @@ import time
 
 # 핸들러
 def process_line(l):
+  time.sleep(5)
   print(l)
 
+
 with open('message.log') as f:
-  while True:
-    line = f.readline()
-    if not line:
-      time.sleep(0.5)
-      continue
-    process_line(line)
+  doc = f.read()
+
+for i in range(len(doc)):
+  with open('command.txt', 'wa') as f:
+    f.write(str(i)+"\n")
+    time.sleep(1)
+
 
 #1. 파일 하나를 읽기 모드로 열었다.
 #2. 한 줄씩 읽어서 process_line() 함수로 보내어 처리한다.
