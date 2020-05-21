3ca968aaab0ce72bc81a21e916b7d572259523ce
hahooon <plmokn1007@kookmin.ac.kr>
2020-05-21 22:14:07 +0900

    #43 model synchronize with preprocessing

--- ml/ml_src/semo.py
+++ ml/ml_src/semo.py
@@ -46,3 +46,65 @@ def preprocessing(data):
         output['now'].extend(now)
         output['id'].extend(ids)
     return output
+
+def concatenate_pred_table(pred, table):
+    for _id in pred.keys():
+        table[_id]['label'] = pred[_id]
+
+
+def labeling(data, predicts, THRESH_HOLD = 0.5):
+    output = {}
+    for predict, _id in zip(predicts, data['id']):
+        if predict > THRESH_HOLD:
+            output[_id] = 1
+        else:
+            output[_id] = 0
+    return output
+
+
+def make_id_table(data):
+    _id_table = {}
+    _type = type(data)
+    if  _type == list:
+        for d in data:
+            temp = {}
+            _id = d["_id"]
+            for key in d.keys():
+                if key != "_id":
+                    temp[key] = d[key]
+            _id_table[_id] = temp
+    return _id_table
+
+
+
+def head_table(table, n=5):
+    for key in list(table)[:n]:
+        print(table[key])
+
+
+def run(
+    LOG_DATA_PATH = "./20181201000000.pkl", GROUPED_KEY = 'orgIDX', 
+    SORT_KEY = "atdate", UNIQUE_ID = '_id', THRESH_HOLD = 0.5
+):
+    """
+    input : data type of input is pandas's DataFrame
+    output : data type of output is dict
+    params : 
+    - GROUPED_KEY : organize index, type is str, int
+    - SORT_KEY    : datetime
+    - UNIQUE_ID   : unique_value of logs
+    """
+    import numpy as np
+    import pandas as pd
+    data = data_loader(LOG_DATA_PATH=LOG_DATA_PATH)
+    _id_table = make_id_table(data)
+    data = pd.DataFrame(data)
+    data = grouped_data(data=data)
+    data = preprocessing(data=data)
+    model = get_model()
+    pred = model.predict({"pre_pay": np.array(data['pre']),
+                          "now_pay": np.array(data['now'])}, verbose=True)
+    pred = labeling(data, pred)
+    concatenate_pred_table(pred, _id_table)
+    
+    return _id_table
