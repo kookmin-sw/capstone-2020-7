d629489b98e0095c215375f7a4f5ead4cc6d90f7
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2020-06-05 00:58:59 +0900

    #61 비로그인시 모든 페이지 접근불가 + 로그아웃 메뉴바에 추가

--- web/mainapp/urls.py
+++ web/mainapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboardtwo/',views.kibana_page2),
     path('Login/',views.Login),
+    path('Logout/',views.Logout),
 ]
--- web/mainapp/views.py
+++ web/mainapp/views.py
@@ -2,33 +2,20 @@ from django.contrib.auth import authenticate
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
 
 @csrf_exempt
-def login(request):
-
-    if request.method == 'POST':
-        id = request.POST.get('userid','')
-        pw = request.POST.get('userpw', '')
-
-        result = authenticate(username=id, password=pw)
-
-        if result :
-            print("로그인 성공!")
-            return render(request, 'mainapp/index.html')
-
-        else:
-            print("실패")
-            return render(request, 'mainapp/login.html')
-
-
-    return render(request, 'mainapp/login.html')
-
 
 def kibana_page(request):
     return render(request, 'mainapp/index.html')
 
 def kibana_page2(request):
     return render(request, 'mainapp/index_for_dashboard2.html')
+
 
 def Login(request):
 
@@ -40,11 +27,15 @@ def Login(request):
 
         if result :
             print("로그인 성공!")
+            login(request, result)
             return render(request, 'mainapp/index.html')
 
         else:
             print("실패")
             return render(request, 'mainapp/Login.html')
 
+    return render(request, 'mainapp/Login.html')
 
+def Logout(request):
+    logout(request)
     return render(request, 'mainapp/Login.html')
